09eae840f15a4309cbcedf09d248590bc3ab49f6
IngabireTina <ingabire067@gmail.com>
2021-03-11 17:16:35 -0500

    created user class and generated new user instance

---
+++ locker.py
@@ -0,0 +1,13 @@
+import random
+
+class User: 
+    # created class user
+
+    users = []
+
+    def__init__(self,username,password):
+
+        #generated new instance of user
+
+        self.username = username
+        self.password = password
