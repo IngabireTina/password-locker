563fa9888826873db20fc4463c259beb93b67dbf
IngabireTina <ingabire067@gmail.com>
2021-03-11 17:52:19 -0500

    added check user exist method

--- locker.py
+++ locker.py
@@ -29,9 +29,23 @@ class User:
         User.users.remove(self)
 
 class Credential():
-    
+
     # Creation of credential class and will help to create new credentials
     credentials = []
 
     def __init__(self, account_name, user_name, account_password):
+     #class instance
+        self.account_name - account_name
+        self.user_name = user_name
+        self.account_password = account_password
 
+    @classmethod
+    def user_checker(cls. username, password):
+        #does user exist?
+
+        for user in User.users:
+            if(user.username == username and user.password == password):
+                user == user.username
+            return user
+
+ 
