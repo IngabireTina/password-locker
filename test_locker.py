4f5b0dafb70beca123e00a576b345b80a401c48e
IngabireTina <ingabire067@gmail.com>
2021-03-11 18:32:33 -0500

    added test to check if user was created correctly

--- test_locker.py
+++ test_locker.py
@@ -9,6 +9,12 @@ class User_Test(unittest.TestCase):
         #function that runs before
         self.user = User('ingabire','ingabire')
 
+    def test_init(self):
+
+        #is user created correctly
+        self.assertEqual(self.user.username,'ingabire')
+        self.assertEqual(self.user.password,'ingabire')
+
 if __name__ == '__main__':
     unittest.main()
         
