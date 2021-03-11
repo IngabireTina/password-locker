b85c1b6a3da15e53b6240b406d0d975373bdbc38
IngabireTina <ingabire067@gmail.com>
2021-03-11 17:45:58 -0500

    added credentials class

--- locker.py
+++ locker.py
@@ -13,15 +13,25 @@ class User:
         self.password = password
 
     def save_user(self):
+
         # function to create user credentials
 
         User.users.append(self)
     @classmethod
     def list_users(cls):
+
         #method to display credentials
         return cls.users
 
     def delete(cls):
+
         #method to delete credentials
         User.users.remove(self)
-        
+
+class Credential():
+    
+    # Creation of credential class and will help to create new credentials
+    credentials = []
+
+    def __init__(self, account_name, user_name, account_password):
+
